48d7667cd15f9bfae1f6f3f0db8e4f3f2bc5b13e
Bryant <bryantkrause@gmail.com>
2024-08-12 16:22:11 -0700

    Found list of Keys

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import geopandas as gpd
 from census import Census
 from us import states
 import os
-from dotenv import load_dotenv, dotenv_values
+from dotenv import dotenv_values
 import requests
+import json
 
 config = dotenv_values(".env")
 key = os.environ.get("APIKEY")
@@ -23,13 +24,26 @@ key = os.environ.get("APIKEY")
 # Put it all together in one f-string:
 # query_url = f"{host}{year}{dataset_acronym}{g}{variables}{location}{usr_key}"
 # realUrl = "https://api.census.gov/data/2019/acs/acs1?get=NAME,B01001_001E&for=us:*&key=732036086eed9681751ca3d7a89bc9af0a7d20d6"
-query_url = "https://api.census.gov/data/2022/acs/acs1/profile?get=group(DP04)&ucgid=pseudo(0100000US$0400000)&key=732036086eed9681751ca3d7a89bc9af0a7d20d6"
-print(query_url)
+# query_url = "https://api.census.gov/data/2022/acs/acs1/profile?get=group(DP04)&ucgid=pseudo(0100000US$0400000)&key=732036086eed9681751ca3d7a89bc9af0a7d20d6"
+titleKey = "https://api.census.gov/data/2019/acs/acs5/profile/groups/DP04.json"
+print(titleKey)
 # Use requests package to call out to the API
-response = requests.get(query_url)
+response = requests.get(titleKey)
 # Convert the Response to text and print the result
 print(response)
 data = response.json()
-print(data)
-df = pd.json_normalize(data)
-pd.DataFrame(data).to_csv("out.csv", index=False)
+print(type(data))
+
+blarg = []
+
+for x, obj in data.items():
+
+    for y in obj:
+        newRow = y , obj[y]["label"]
+        blarg.append(newRow)
+
+print(len(blarg))
+df = pd.DataFrame(blarg)
+print(df.head())
+print(df.shape)
+pd.DataFrame(df).to_csv("dp04Group.csv", index=False)
